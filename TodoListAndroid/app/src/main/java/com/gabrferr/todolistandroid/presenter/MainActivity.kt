108a608f342133cd7f24d16483fa89237fe38a69
Gabriel Ferreira <gabriel9892@gmail.com>
2022-09-23 19:19:04 -0300

    Adicionados exercicios realizados no dia 23/09

---
+++ TodoListAndroid/app/src/main/java/com/gabrferr/todolistandroid/presenter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gabrferr.todolistandroid.presenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gabrferr.todolistandroid.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
